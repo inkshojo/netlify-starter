94aefc992d7a2a2e6f29bccff4ecacc76f25d16d
inkshojo <lesliegreenmeteor@gmail.com>
2025-11-06 13:19:43 -0800

    Update comment.js

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -1,33 +1,11 @@
-// netlify/functions/comment.js
 const http = require('http');
 const Waline = require('@waline/vercel');
 const serverless = require('serverless-http');
 
 const app = Waline({
   env: 'netlify',
-
-  // -----------------------------
-  // 1. Disable automatic geolocation
-  // -----------------------------
-  useIPLocation: false,   // prevents location from being computed from IP
-
-  // -----------------------------
-  // 2. Clear sensitive fields before saving
-  // -----------------------------
-  preSave(comment) {
-    // Remove IP, UA, location for privacy
-    comment.ip = '';
-    comment.ua = '';
-    comment.location = '';
-    // Return a resolved promise to avoid Netlify async issues
-    return Promise.resolve(comment);
-  },
-
-  // -----------------------------
-  // 3. Optional postSave hook
-  // -----------------------------
-  postSave(comment) {
-    // For analytics/logging only if needed
+  async postSave(comment) {
+    // do what ever you want after save comment
   },
 });
 
