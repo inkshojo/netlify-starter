420eb5dc6322bbbd50241583e04809c02f34c6df
inkshojo <lesliegreenmeteor@gmail.com>
2025-11-06 09:59:17 -0800

    Add files via upload

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -1,11 +1,23 @@
 const http = require('http');
-const Waline = require('@waline/vercel');
+const { Waline } = require('@waline/vercel');
 const serverless = require('serverless-http');
 
+// Waline 配置
 const app = Waline({
   env: 'netlify',
+
+  // 保存前清除敏感信息
+  async preSave(comment) {
+    // 删除 IP、地理位置、UA
+    comment.ip = '';
+    comment.location = '';
+    comment.ua = '';
+    return comment;
+  },
+
+  // 保存后（可选）
   async postSave(comment) {
-    // do what ever you want after save comment
+    // 保存后你想执行的逻辑（可留空）
   },
 });
 
