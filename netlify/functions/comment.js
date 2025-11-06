c1cdfe53eb87f188374c811b981cce9d7214716b
inkshojo <lesliegreenmeteor@gmail.com>
2025-11-06 10:43:05 -0800

    Update comment function for privacy enhancements
    Added configuration to disable geolocation and clear sensitive fields before saving comments.

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -1,3 +1,4 @@
+// netlify/functions/comment.js
 const http = require('http');
 const Waline = require('@waline/vercel');
 const serverless = require('serverless-http');
@@ -5,14 +6,28 @@ const serverless = require('serverless-http');
 const app = Waline({
   env: 'netlify',
 
+  // -----------------------------
+  // 1. Disable automatic geolocation
+  // -----------------------------
+  useIPLocation: false,   // prevents location from being computed from IP
+
+  // -----------------------------
+  // 2. Clear sensitive fields before saving
+  // -----------------------------
   preSave(comment) {
+    // Remove IP, UA, location for privacy
     comment.ip = '';
-    comment.location = '';
     comment.ua = '';
+    comment.location = '';
+    // Return a resolved promise to avoid Netlify async issues
     return Promise.resolve(comment);
   },
 
-  async postSave(comment) {
+  // -----------------------------
+  // 3. Optional postSave hook
+  // -----------------------------
+  postSave(comment) {
+    // For analytics/logging only if needed
   },
 });
 
