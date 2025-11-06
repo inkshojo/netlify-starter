91f0fa0fae605dafa159d748c23241338762a647
inkshojo <lesliegreenmeteor@gmail.com>
2025-11-06 10:31:37 -0800

    Change preSave to postSave in comment function

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -1,18 +1,16 @@
-// netlify/functions/comment.js
 const http = require('http');
 const Waline = require('@waline/vercel');
 const serverless = require('serverless-http');
 
 const app = Waline({
   env: 'netlify',
-
-async preSave(comment) {
-  comment.ip = '';
-  comment.location = '';
-  comment.ua = '';
-  return comment; // 必须返回
-}
-,
+  async postSave(comment) {
+    // do what ever you want after save comment
+    comment.ip = '';
+    comment.location = '';
+    comment.ua = '';
+    return comment;
+  },
 });
 
 module.exports.handler = serverless(http.createServer(app));
